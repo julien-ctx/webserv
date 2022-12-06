822b50e87d5795ddacc6f98e59698d2e08ac091d
Julien CAUCHETEUX <jdaodkql@gmail.com>
2022-12-06 17:29:39 +0100

    Upload totally finished, added HTML

--- www/cgi/upload.py
+++ www/cgi/upload.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	boundary = stdin.split('\n')[0]
 	stdin = stdin.split(boundary)
 	content = ""
-	msg = "The file has been successfully uploaded"
+	msg = "File uploaded"
 
 	for block in stdin:
 		if "filename=" in block:
@@ -21,11 +21,43 @@ if __name__ == "__main__":
 				if "filename=" in line:
 					filename = line.split('filename="', 1)[1].split('"')[0]
 			if content and filename:
+				if not os.path.isdir("./www/cgi/uploads/"):
+					msg = "Upload directory doesn't exists"
 				if (os.path.isfile("./www/cgi/uploads/" + filename)):
-					msg = "A file with the same name already exists!"
+					msg = "File already exists!"
 				with open("./www/cgi/uploads/" + filename, 'w') as file:
 					file.write(content)
 			else:
-				msg = "Filename or file data is invalid!"
+				msg = "Invalid submission"
 
-	print(msg)
+	html = """
+	<!DOCTYPE html>
+	<html lang="en" >
+
+	<head>
+	<meta charset="UTF-8">
+	<title>Webserv | Python CGI</title>
+	<link rel="stylesheet" type="text/css" href="../style.css"/>
+	<link rel="icon" href="../images/favicon.ico" type="image/x-icon" />
+	</head>
+
+	<h1>""" + msg + """</h1>
+	<div class="center">
+	<a href="/">
+		<button class="home-button">Home</button>
+	</a>
+	</div>
+	<footer>
+		<div class="wave"></div>
+		<div class="wave"></div>
+		<div class="wave"></div>
+		<h2><a href="https://github.com/ktroude" target="_blank">ktroude</a>
+			| <a href="https://github.com/YeetusTitus" target="_blank">jforner</a>
+			| <a href="https://github.com/julien-ctx" target="_blank">jcauchet</a>
+		</h2>
+	</footer>
+	</body>
+
+	</html>
+	"""
+	print(html)
